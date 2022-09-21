7442ae5251a545892b0371d8eed70cd779652649
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-09-21 11:43:40 +0200

    Add AttackWeapon and DefensiveWeapon classes, the Player armor and damage can grow with weapons and armors

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Sword.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Sword.java
@@ -1,21 +1,17 @@
 package com.codecool.dungeoncrawl.logic.items;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
-public class Sword extends Item{
-    private int damage;
+public class Sword extends AttackWeapon {
 
     public Sword(Cell cell) {
         super(cell);
-        this.damage = 10;
-    }
-
-    public int getDamage() {
-        return damage;
+        this.damage = 5;
     }
 
     @Override
     public String getTileName() {
-        return "sword";
+        return CellType.SWORD.getTileName();
     }
 }

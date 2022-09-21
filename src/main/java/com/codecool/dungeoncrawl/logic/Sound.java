cc85a2da1b4af992a6fc4c7cf45895bcbdfc8d55
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-09-21 15:12:46 +0200

    Add music files and generalize their filepath

--- src/main/java/com/codecool/dungeoncrawl/logic/Sound.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Sound.java
@@ -1,22 +1,27 @@
 package com.codecool.dungeoncrawl.logic;
 
 public enum Sound {
-    BASIC_ATTACK("//home//g-sarkadi//Desktop//basic_attack.wav"),
-    ADVANCED_ATTACK("//home//g-sarkadi//Desktop//advanced_attack.wav"),
-    DOOR("//home//g-sarkadi//Desktop//door.wav"),
-    MOVE("//home//g-sarkadi//Desktop//move.wav"),
-    PICK_UP_ITEM("//home//g-sarkadi//Desktop//pickup.wav"),
-    SKELETON_SOUND("//home//g-sarkadi//Desktop//skeleton.wav"),
-    DEATH("//home//g-sarkadi//Desktop//death.wav"),
-    MUSIC("//home//g-sarkadi//Desktop//music.wav");
+    BASIC_ATTACK("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//basic_attack.wav"),
+    ADVANCED_ATTACK("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//advanced_attack.wav"),
+    DOOR("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//door.wav"),
+    MOVE("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//move.wav"),
+    PICK_UP_ITEM("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//pickup.wav"),
+    SKELETON_SOUND("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//skeleton.wav"),
+    DEATH("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//death.wav"),
+    MUSIC("//home//" + getUsername() + "//codecool_projects//03_oop//03_week_pair//dungeon-crawl-1-java-NagyGabor87//src//main//resources//music.wav");
 
     private final String filePath;
 
     Sound(String filePath) {
         this.filePath = filePath;
+
     }
 
     public String getFilePath() {
         return filePath;
+    }
+
+    private static String getUsername() {
+        return System.getProperty("user.name");
     }
 }

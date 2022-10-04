91d6aaf75b08081cfefaba088bc4e09a8ad58135
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-10-04 15:30:08 +0200

    Remove dead code in main from the second run skeleton

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -37,17 +37,12 @@ public class Main extends Application {
     GameMap map = MapLoader.loadMap(level.getMapLevel(), null);
     GridPane ui = new GridPane();
 
-
-    //public class Main extends Application {
-//    GameMap map = MapLoader.loadMap();
     Canvas canvas = new Canvas(
             map.getWidth() * Tiles.TILE_WIDTH,
             map.getHeight() * Tiles.TILE_WIDTH);
     GraphicsContext context = canvas.getGraphicsContext2D();
     Label healthLabel = new Label();
     GameDatabaseManager dbManager;
-
-//    public static void main(String[] args) {
 
     public static void main(String[] args) {
         launch(args);
@@ -56,10 +51,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage){
         setupDbManager();
-//        GridPane ui = new GridPane();
-//        ui.setPrefWidth(200);
-//        ui.setPadding(new Insets(10));
-
 
         ui.setPrefWidth(200);
         ui.setPadding(new Insets(10));
@@ -90,9 +81,6 @@ public class Main extends Application {
                 || keyEvent.getCode() == KeyCode.ESCAPE) {
             exit();
         }
-
-//        primaryStage.setTitle("Dungeon Crawl");
-//        primaryStage.show();
     }
     private void exit () {
         try {
@@ -123,18 +111,15 @@ public class Main extends Application {
                 break;
             case SPACE:
                 map.addInventory();
-//                refresh();
                 break;
             case I:
                 clearInventoryText();
                 String inventory = map.getPlayer().inventoryToString();
                 ui.add(new Label("Inventory:"), 0, 1);
                 ui.add(new Label(inventory), 1, 1);
-//                refresh();
                 break;
             case ESCAPE:
                 clearInventoryText();
-//                refresh();
                 break;
             case S:
                 Player player = map.getPlayer();
@@ -162,7 +147,6 @@ public class Main extends Application {
     }
 
     private void refresh() {
-        System.out.println(map.toString()); // todo remove
         moveMonsters();
         removeDisappearingWall();
         drawMap();
@@ -197,40 +181,7 @@ public class Main extends Application {
             }
         }
         healthLabel.setText("" + map.getPlayer().getHealth());
-//        case UP:
-//        map.getPlayer().move(0, -1);
-//        refresh();
-//        break;
-//        case DOWN:
-//        map.getPlayer().move(0, 1);
-//        refresh();
-//        break;
-//        case LEFT:
-//        map.getPlayer().move(-1, 0);
-//        refresh();
-//        break;
-//        case RIGHT:
-//        map.getPlayer().move(1, 0);
-//        refresh();
-//        break;
-//    }
     }
-
-//    private void refresh() {
-//        context.setFill(Color.BLACK);
-//        context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-//        for (int x = 0; x < map.getWidth(); x++) {
-//            for (int y = 0; y < map.getHeight(); y++) {
-//                Cell cell = map.getCell(x, y);
-//                if (cell.getActor() != null) {
-//                    Tiles.drawTile(context, cell.getActor(), x, y);
-//                } else {
-//                    Tiles.drawTile(context, cell, x, y);
-//                }
-//            }
-//        }
-//        healthLabel.setText("" + map.getPlayer().getHealth());
-//    }
 
     private void stepNextLevel() {
         if (!map.getPlayer().isAlive()) {

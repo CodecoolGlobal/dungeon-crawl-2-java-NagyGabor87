c07e5e54bb9f269159fe9772a9cebfd49ac74937
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-10-04 15:26:41 +0200

    Add queries to PlayerDaoJdbc

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -4,10 +4,11 @@ import com.codecool.dungeoncrawl.model.PlayerModel;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerDaoJdbc implements PlayerDao {
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public PlayerDaoJdbc(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -34,16 +35,65 @@ public class PlayerDaoJdbc implements PlayerDao {
 
     @Override
     public void update(PlayerModel player) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "UPDATE player SET player_name = ?, hp = ?, x = ?, y = ?, inventory = ? WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, player.getPlayerName());
+            statement.setInt(2, player.getHp());
+            statement.setInt(3, player.getX());
+            statement.setInt(4, player.getY());
+            statement.setString(5, player.getInventory());
+            statement.setInt(6, player.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public PlayerModel get(int id) {
-        return null;
+        PlayerModel player;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, player_name, hp, x, y, inventory FROM player WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()) {
+                return null;
+            }
+
+            int player_id = resultSet.getInt(1);
+            String player_name = resultSet.getString(2);
+            int hp = resultSet.getInt(3);
+            int x = resultSet.getInt(4);
+            int y = resultSet.getInt(5);
+            String inventory = resultSet.getString(6);
+
+            player = new PlayerModel(player_name, x, y, inventory);
+            player.setId(player_id);
+            player.setHp(hp);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return player;
     }
 
     @Override
     public List<PlayerModel> getAll() {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, player_name, hp, x, y, inventory FROM player";
+            ResultSet resultSet = conn.createStatement().executeQuery(sql);
+            List<PlayerModel> players = new ArrayList<>();
+            while (resultSet.next()) {
+                PlayerModel player = new PlayerModel(resultSet.getString(2), resultSet.getInt(4), resultSet.getInt(4),resultSet.getString(6));
+                player.setId(resultSet.getInt(1));
+                player.setHp(resultSet.getInt(3));
+                players.add(player);
+            }
+            return players;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
